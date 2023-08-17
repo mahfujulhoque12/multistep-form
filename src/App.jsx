9ee75bc55941f680102bb12b474769f9f5346524
Mahfujul-hoque <mdmahfujulhoque123@gmail.com>
2023-08-17 13:25:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from './component/Form'
+const App = () => {
+  return (
+    <div>
+   <Form></Form>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/SignUpInfo.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function SignUpInfo({ formData, setFormData }) {
+  return (
+    <div className="sign-up-container">
+      <input
+        type="text"
+        placeholder="Project Name"
+        value={formData.email}
+        onChange={(event) =>
+          setFormData({ ...formData, email: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Project Description"
+        value={formData.password}
+        onChange={(event) =>
+          setFormData({ ...formData, password: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Client"
+        value={formData.confirmPassword}
+        onChange={(event) =>
+          setFormData({ ...formData, confirmPassword: event.target.value })
+        }
+      />
+       <input
+        type="text"
+        placeholder="Contractor"
+        value={formData.confirmPassword}
+        onChange={(event) =>
+          setFormData({ ...formData, confirmPassword: event.target.value })
+        }
+      />
+    </div>
+  );
+}
+
+export default SignUpInfo;
